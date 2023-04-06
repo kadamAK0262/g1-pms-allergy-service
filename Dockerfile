b01a859c8f8d3ab548f97a8918282d9a10f66bf6
kadamAK0262 <akshaykadam0262@gmail.com>
2023-04-06 16:53:41 +0530

    for delete

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM openjdk:17
-EXPOSE 8080
-ADD target/g1-pms-allergy-docker.jar g1-pms-allergy-docker.jar
-ENTRYPOINT [ "java","-jar","/g1-pms-allergy-docker.jar" ]
--- src/main/java/com/revature/pms/repo/AllergyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.revature.pms.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.revature.pms.model.Allergy;
-
-public interface AllergyRepository extends JpaRepository<Allergy, Integer> {
-
-}
